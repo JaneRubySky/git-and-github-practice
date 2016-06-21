8e7a9f2f28b1ae6095707c70a41e6eee0b8409ac
Jane Ruby <janerubyskywalker@gmail.com>
2016-06-21 17:21:46 -0400

    added my part

--- src/com/company/Car.java
+++ src/com/company/Car.java
@@ -4,4 +4,10 @@ package com.company;
  * Created by administrator on 6/21/16.
  */
 public class Car {
+
+    public static String number1;
+    public static String number2;
+    public static String number3;
+    
 }
+
---
+++ src/com/company/Garage.java
@@ -0,0 +1,8 @@
+package com.company;
+
+/**
+ * Created by janerubygrissom on 6/21/16.
+ */
+public class Garage {
+}
+
